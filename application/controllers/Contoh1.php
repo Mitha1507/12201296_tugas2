58fb0347409579a9b1068feb9d976c60bc93ab16
Mitha1507 <silvianamitha57@gmail.com>
2021-09-25 12:15:20 +0700

    menambahkan framework

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,14 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan</h1>";
+        echo "Nama saya Mitha Silviana
+            saya tinggal di kp.Gadog rt/rw 004/011
+            nomor telpon saya 089524796773
+            olahraga yang saya sukai adalah
+            Bulutangkis";
+    }
+}
+?>
